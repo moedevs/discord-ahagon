05990f2ba010104e12fdf6c97c92283cfcf62cd4
Xetera <mythologyali@gmail.com>
2019-03-05 21:59:17 -0800

    cleaned up parser

--- src/parsers/parsimmon.ts
+++ src/parsers/parsimmon.ts
@@ -1,8 +1,9 @@
 import P from "parsimmon";
-import { Argument } from "../../main";
+import { Argument, ParserOptions } from "../../main";
 
 export const enum ArgType {
   MEMBER_MENTION = "member_mention",
+  BOOLEAN = "boolean",
   PREFIX = "prefix",
   COMMAND = "command",
   CHANNEL_MENTION = "channel_mention",
@@ -15,7 +16,7 @@ export const enum ArgType {
   TEXT = "text",
 }
 
-const USERS_PATTERN = /<@!?[0-9]+>/;
+const USERS_PATTERN = /<@!?([0-9])+>/;
 const CHANNELS_PATTERN = /<#[0-9]+>/;
 const ROLES_PATTERN = /<@&[0-9]+>/;
 
@@ -23,25 +24,93 @@ const lexeme = <T>(parser: P.Parser<T>) => parser.skip(P.optWhitespace);
 
 const repeatArg = <T>(parser: P.Parser<T>) => parser.sepBy(P.string(" "));
 
-const createCommandParser = (args: Argument[]) => P.createLanguage({
-  quote: () => P.string('"'),
-  [ArgType.WORD]: () => P.letters.node("word"),
-  [ArgType.FLAG]: () => P.alt(P.string("--"), P.string("-")).then(P.letters),
-  [ArgType.MEMBER_MENTION]: () => P.regex(USERS_PATTERN).node("member_mention"),
-  [ArgType.CHANNEL_MENTION]: () => P.regex(CHANNELS_PATTERN).node("channel_mention"),
-  [ArgType.ROLE_MENTION]: () => P.regex(ROLES_PATTERN).node("role_mention"),
-  [ArgType.NUMBER]: () => P.digits.map(Number).node("number"),
-  [ArgType.STRING]: (r: P.Language) => P.alt(r.quoted_string, r.word),
-  [ArgType.QUOTED_STRING]: (r: P.Language) =>
-    r.quote
+const truthy = ["yes", "true", "1", "on"];
+const falsy = ["no", "false", "0", "off"];
+
+const createCommandParser = (args: Argument[], opts: ParserOptions) => P.createLanguage({
+  quote: () => {
+    return P.string('"');
+  },
+  true: () => {
+    return P.alt(...truthy.map(P.string))
+      .result(true)
+      .desc("true");
+  },
+  false: () => {
+    return P.alt(...falsy.map(P.string))
+      .result(false)
+      .desc("false");
+  },
+  [ArgType.BOOLEAN]: (r: P.Language) => {
+    return P.alt(r.true, r.false)
+      .node("boolean")
+      .desc(ArgType.BOOLEAN);
+  },
+  [ArgType.WORD]: () => {
+    return P.letters
+      .node("word")
+      .desc(ArgType.WORD);
+  },
+  [ArgType.FLAG]: () => {
+    return P.alt(P.string("--"), P.string("-"))
+      .then(P.letters)
+      .desc(ArgType.FLAG);
+  },
+  [ArgType.MEMBER_MENTION]: () => {
+    return P.regex(USERS_PATTERN)
+      .node("member_mention")
+      .desc(ArgType.MEMBER_MENTION);
+  },
+  [ArgType.CHANNEL_MENTION]: () => {
+    return P.regex(CHANNELS_PATTERN)
+      .node("channel_mention")
+      .desc(ArgType.CHANNEL_MENTION);
+  },
+  [ArgType.ROLE_MENTION]: () => {
+    return P.regex(ROLES_PATTERN)
+      .node("role_mention")
+      .desc(ArgType.ROLE_MENTION);
+  },
+  [ArgType.NUMBER]: () => {
+    return P.regex(/[0-9]+/)
+      .node("number")
+      .desc(ArgType.NUMBER);
+  },
+  [ArgType.STRING]: (r: P.Language) => {
+    return P.alt(r.quoted_string, r.word)
+      .desc(ArgType.STRING);
+  },
+  [ArgType.QUOTED_STRING]: (r: P.Language) => {
+    return r.quote
       .then(P.noneOf('"').many().tie())
       .skip(r.quote)
-      .node("quoted_string"),
-  [ArgType.TEXT]: () => P.all.node("text"),
-  [ArgType.COMMAND]: () => P.regex(/(\w|\d)+/).node("command"),
-  [ArgType.PREFIX]: () => P.string("!").node("prefix"),
+      .node("quoted_string")
+      .desc(ArgType.QUOTED_STRING);
+  },
+  [ArgType.TEXT]: () => {
+    return P.all
+      .node("text")
+      .desc(ArgType.TEXT);
+  },
+  [ArgType.COMMAND]: () => {
+    return P.regex(/(\w|\d)+/)
+      .node("command")
+      .desc(ArgType.COMMAND);
+  },
+  [ArgType.PREFIX]: (r: P.Language) => {
+    const prefixParser = P.string(opts.prefix);
+    const parser = opts.mentionPrefix
+      ? P.alt(r.member_mention, prefixParser)
+      : prefixParser;
+    return parser.node("prefix").desc(ArgType.PREFIX);
+  },
   parser: (r) => {
-    const parsers = args.map((arg) => {
+    const prefix: Argument = {
+      type: ArgType.PREFIX,
+      name: "prefix",
+    };
+
+    const parsers = [prefix, ...args].map((arg) => {
       const parser = r[arg.type];
       if (arg.repeat) {
         // repeat args are guaranteed to be the last
@@ -49,27 +118,29 @@ const createCommandParser = (args: Argument[]) => P.createLanguage({
       }
       return lexeme(parser);
     });
+
     return P.seq(...parsers);
-  },
+  }
 });
 
-const alwaysArgs = [{
-  type: ArgType.PREFIX,
-  name: "prefix",
-}, {
+const input: Argument[] = [{
   type: ArgType.COMMAND,
   name: "command",
-}];
-
-const input: Argument[] = [{
-  type: ArgType.NUMBER,
-  name: "count",
 }, {
   type: ArgType.NUMBER,
   name: "count",
-  repeat: true,
+}, {
+  type: ArgType.BOOLEAN,
+  name: "thing",
 }];
 
-const muteParser = createCommandParser([...alwaysArgs, ...input]).parser;
+const muteParser = createCommandParser(input, { prefix: "!", mentionPrefix: true }).prefix;
 
-const e = muteParser.tryParse("!mute 60 60 06 060 66 000650");
+const src = "<@12315412>mute 60";
+const e = muteParser.parse(src);
+
+if (e.status) {
+  console.log(e);
+} else {
+  console.log(e);
+}
--- src/parsers/resolvers.ts
+++ src/parsers/resolvers.ts
@@ -1,50 +1,64 @@
-import { Command, Context } from '../../main'
 import * as Discord from "discord.js";
+import { Command, Context } from "../../main";
 
 const getMemberFromID = (text: string, ctx: Context): Discord.GuildMember | undefined => {
-  if (!text || !ctx.message.guild) return
-  return ctx.message.guild.members.get(text)
-}
+  if (!text || !ctx.message.guild) {
+    return;
+  }
+  return ctx.message.guild.members.get(text);
+};
 
 const getMemberFromName = (text: string, ctx: Context): Discord.GuildMember | undefined => {
-  if (!ctx.message.guild) return
-  return ctx.message.guild.members.find(member => {
-    return member.user.username.startsWith(text) || member.nickname.startsWith(text)
-  })
-}
+  if (!ctx.message.guild) {
+    return;
+  }
+  return ctx.message.guild.members.find((member) => {
+    return member.user.username.startsWith(text) || member.nickname.startsWith(text);
+  });
+};
 
 const getTextChannelFromID = (text: string, ctx: Context): Discord.TextChannel | undefined => {
-  if (!ctx.message.guild) return
-  return ctx.message.guild.channels.get(text) as Discord.TextChannel | undefined
-}
+  if (!ctx.message.guild) {
+    return;
+  }
+  return ctx.message.guild.channels.get(text) as Discord.TextChannel | undefined;
+};
 
 const getTextChannelFromName = (text: string, ctx: Context): Discord.TextChannel | undefined => {
-  if (!ctx.message.guild) return
-  return ctx.message.guild.channels.find(channel => {
-    return channel.name === text && channel instanceof Discord.TextChannel
-  }) as Discord.TextChannel | undefined
-}
+  if (!ctx.message.guild) {
+    return;
+  }
+  return ctx.message.guild.channels.find((channel) => {
+    return channel.name === text && channel instanceof Discord.TextChannel;
+  }) as Discord.TextChannel | undefined;
+};
 
 const getRoleFromID = (text: string, ctx: Context): Discord.Role | undefined => {
-  if (!ctx.message.guild) return
-  return ctx.message.guild.roles.get(text)
-}
+  if (!ctx.message.guild) {
+    return;
+  }
+  return ctx.message.guild.roles.get(text);
+};
 
 const getRoleFromName = (text: string, ctx: Context): Discord.Role | undefined => {
-  if (!ctx.message.guild) return
-  return ctx.message.guild.roles.find(role => {
-    return role.name.startsWith(text)
-  })
-}
+  if (!ctx.message.guild) {
+    return;
+  }
+  return ctx.message.guild.roles.find((role) => {
+    return role.name.startsWith(text);
+  });
+};
 
 const getEmojiFromString = (text: string, ctx: Context): Discord.Emoji | undefined => {
-  if (!ctx.message.guild) return
-  const regex = /<a?:[a-zA-Z0-9]+:([0-9]+)>/g
-  const match = regex.exec(text)
-  if (match != null) {
-    text = match[1]
+  if (!ctx.message.guild) {
+    return;
   }
-  return ctx.message.guild.emojis.get(text)
-}
+  const regex = /<a?:[a-zA-Z0-9]+:([0-9]+)>/g;
+  const match = regex.exec(text);
+  if (match) {
+    text = match[1];
+  }
+  return ctx.message.guild.emojis.get(text);
+};
 
 const getCommandFromName = (commands: Discord.Collection<string, Command>, input: string) => commands.get(input);
